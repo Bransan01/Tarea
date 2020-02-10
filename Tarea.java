d45ca455c12bba559fd354d0361b7a0cd24490ce
Bransan01 <brandonsantos001@gmail.com>
2020-02-09 20:02:28 -0600

    first Version

---
+++ Tarea.java
@@ -0,0 +1,22 @@
+var mes == "12"
+console.log(mes);
+if(mes == 12 || (mes => 1 && mes <=2)){
+document.write('es invierno');
+}else if (mes == 3){
+document.write("es primavera");
+}else if (mes == 4){
+document.write("es primavera");
+}else if (mes == 5){
+document.write("es primavera");
+}else if (mes == 6){
+document.write("es verano");
+}else if (mes == 7){
+document.write("es verano");
+}else if (mes == 8){
+document.write("es verano");
+}else if (mes == 9){
+document.write("es otoño");
+}else if (mes == 10){
+document.write("es otoño");
+}else if (mes == 11){
+document.write("es otoño");
